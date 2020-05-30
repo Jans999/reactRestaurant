b64e0423ec438ccf3066c6505a14083b9ad24ae5
Jannick Moth <jannickmoth@hotmail.co.uk>
2020-05-30 12:14:49 +0100

    form styled and validation done

--- src/components/BookingModal.js
+++ src/components/BookingModal.js
@@ -1,31 +1,195 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { LocalForm, Control, Errors } from 'react-redux-form';
+import styled from 'styled-components'
 
-const ModalExample = (props) => {
-  const {
-    buttonLabel,
-    className
-  } = props;
+// Validators
+const required = (val) => val && val.length;
+const validEmail = (val) => /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(val);
 
-  const [modal, setModal] = useState(false);
 
-  const toggle = () => setModal(!modal);
 
-  return (
-    <div>
-      <Button color="danger" onClick={toggle}>{buttonLabel}Book</Button>
-      <Modal isOpen={modal} toggle={toggle} className={className}>
-        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
-        <ModalBody>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-        </ModalBody>
-        <ModalFooter>
-          <Button color="primary" onClick={toggle}>Do Something</Button>{' '}
-          <Button color="secondary" onClick={toggle}>Cancel</Button>
-        </ModalFooter>
+export default class BookingModal extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {
+      modalOpen: false,
+      tableId: "",
+    }
+  }
+
+  // Add validators
+
+  handleChange = (e) => { this.setState({[e.target.name]: e.target.value})   }
+
+  // handleUpdate(form) { ... }
+
+  handleSubmit = (values) => {
+
+    console.log('Current State is: ' + JSON.stringify(values));
+    alert('Current State is: ' + JSON.stringify(values));
+    this.toggle();
+
+    // this.setState({tableId: this.props.id})
+    // console.log("submit working")
+    // console.log(values) 
+    // this.toggle()
+  }
+
+  // Bridging tasks
+  // Get validation working
+  // Get passing of table ID working
+
+
+
+  toggle = () => this.setState(prevstate => ({modalOpen: !prevstate.modalOpen}));
+
+  render() {
+
+
+    return (
+      <div>
+      <Button color="danger" onClick={this.toggle}>Book</Button>
+
+        <Modal isOpen={this.state.modalOpen} toggle={this.stoggle} className="">
+        <LocalForm
+        // onUpdate={(form) => this.handleUpdate(form)}
+        onSubmit={(values) => this.handleSubmit(values)}
+      >
+
+          <ModalHeader toggle={this.toggle}>Book table {this.props.id}</ModalHeader>
+          <ModalBody>
+          <BookingForm>
+
+          <div className="field">
+            <label htmlFor="firstname">First Name:</label>
+            <Control.text 
+              model=".firstName"
+              id="firstName"
+              name="firstName"
+              className="form-control"
+              validators={{
+                required 
+            }} />
+             <Errors
+                className="text-danger"
+                model=".firstName"
+                show="touched"
+                messages={{
+                    required: 'Required'
+              }} />
+          </div>
+
+          <div className="field">
+            <label>Last Name:</label>
+            <Control.text
+              className="form-control"
+              onChange={(e) => this.handleChange(e)} model=".lastName" 
+              validators={{
+                required 
+            }}
+              />
+              <Errors
+                className="text-danger"
+                model=".lastName"
+                show="touched"
+                messages={{
+                    required: 'Required'
+              }} />
+          </div>
+
+          <div className="field">
+            <label>Email:</label>
+            <Control.text 
+              onChange={(e) => this.handleChange(e)} 
+              model=".emailAddress" 
+              type="email" 
+              className="form-control"
+              validators={{
+                validEmail
+            }}
+              />
+            <Errors
+                className="text-danger"
+                model=".emailAddress"
+                show="touched"
+                messages={{
+                    validEmail: 'Please enter a valid email address'
+              }} />
+          </div>
+
+          <div className="field">
+            <label>Date:</label>
+            <Control.text 
+            onChange={(e) => this.handleChange(e)}
+             model=".date" 
+             type="date" 
+             validators={{
+                required
+            }}
+             />
+              <Errors
+                className="text-danger"
+                model=".date"
+                show="touched"
+                messages={{
+                    required: 'Required'
+              }} />
+          </div>
+
+          <div className="field">
+            <label>Time:</label>
+            <Control.select model=".time" 
+            onChange={(e) => this.handleChange(e)}
+            validators={{
+              required
+            }}>
+              <option value="17:00">5pm</option>
+              <option value="18.00">6pm</option>
+              <option value="19.00">7pm</option>
+              <option value="20.00">8pm</option>
+            </Control.select>
+            <Errors
+                className="text-danger"
+                model=".time"
+                show="touched"
+                messages={{
+                    required: 'Required'
+              }} />
+          </div>
+
+          
+         
+          <div className="field">
+            <label>Notes:</label>
+            <Control.textarea onChange={(e) => this.handleChange(e)} model=".notes" />
+          </div>
+          </BookingForm>
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary">Do Something</Button>{' '}
+            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+          </ModalFooter>
+
+      </LocalForm>
       </Modal>
-    </div>
-  );
+
+      </div>
+    );
+  }
 }
 
-export default ModalExample;
+const BookingForm = styled.div`
+  .field input,
+  .field textarea,
+  .field select {
+    width: 100%;
+    padding: 0.3em;
+  }
+
+  .field textarea {
+    padding: 2em 0.3em;
+  }
+
+`
--- src/components/TableBooking.js
+++ src/components/TableBooking.js
@@ -13,15 +13,15 @@ export default class TableBooking extends Component {
                 <h1 className="text-center my-3">Book a table</h1>
                 <Container className="text-center">
                     <Row className="text-center mx-auto">
-                        <Col md="4" className="py-2"><Table>1 <BookingModal/></Table></Col>
-                        <Col md="4" className="py-2"><Table>2 <BookingModal/></Table></Col>
-                        <Col md="4" className="py-2"><Table>3 <BookingModal/></Table></Col>
-                        <Col md="4" className="py-2"><Table>4 <BookingModal/></Table></Col>
-                        <Col md="4" className="py-2"><Table>5 <BookingModal/></Table></Col>
-                        <Col md="4" className="py-2"><Table>6 <BookingModal/></Table></Col>
-                        <Col md="4" className="py-2"><Table>7 <BookingModal/></Table></Col>
-                        <Col md="4" className="py-2"><Table>8 <BookingModal/></Table></Col>
-                        <Col md="4" className="py-2"><Table>9 <BookingModal/></Table></Col>
+                        <Col md="4" className="py-2" id="1"><Table>1 <BookingModal id="1" /></Table></Col>
+                        <Col md="4" className="py-2" id="2"><Table>2 <BookingModal id="2" /></Table></Col>
+                        <Col md="4" className="py-2" id="3"><Table>3 <BookingModal id="3" /></Table></Col>
+                        <Col md="4" className="py-2" id="4"><Table>4 <BookingModal id="4" /></Table></Col>
+                        <Col md="4" className="py-2" id="5"><Table>5 <BookingModal id="5" /></Table></Col>
+                        <Col md="4" className="py-2" id="6"><Table>6 <BookingModal id="6" /></Table></Col>
+                        <Col md="4" className="py-2" id="7"><Table>7 <BookingModal id="7" /></Table></Col>
+                        <Col md="4" className="py-2" id="8"><Table>8 <BookingModal id="8" /></Table></Col>
+                        <Col md="4" className="py-2" id="9"><Table>9 <BookingModal id="9" /></Table></Col>
 
                     </Row>
                 </Container>
